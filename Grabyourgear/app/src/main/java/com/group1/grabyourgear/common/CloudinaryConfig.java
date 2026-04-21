b96a63bdf1f99566ba70749486c8659a11086f8a
sifaz <sifazhang.nz@gmail.com>
2026-04-22 10:05:01 +1200

    upload CloudinaryConfig again

---
+++ Grabyourgear/app/src/main/java/com/group1/grabyourgear/common/CloudinaryConfig.java
@@ -0,0 +1,9 @@
+package com.group1.grabyourgear.common;
+
+import com.group1.grabyourgear.BuildConfig;
+
+public class CloudinaryConfig {
+    public static final String CLOUD_NAME = BuildConfig.CLOUDINARY_CLOUD_NAME;
+    public static final String API_KEY = BuildConfig.CLOUDINARY_API_KEY;
+    public static final String API_SECRET = BuildConfig.CLOUDINARY_API_SECRET;
+}
