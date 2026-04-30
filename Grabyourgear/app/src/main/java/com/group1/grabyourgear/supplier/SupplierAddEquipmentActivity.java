d566394c00507c2e84cfa97db78b291b07451234
Amelie Hemmerich <amelie.hemmerich@outlook.de>
2026-04-30 15:05:55 +1200

    Adding all basic supplier activities

---
+++ Grabyourgear/app/src/main/java/com/group1/grabyourgear/supplier/SupplierAddEquipmentActivity.java
@@ -0,0 +1,165 @@
+package com.group1.grabyourgear.supplier;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.group1.grabyourgear.R;
+import com.group1.grabyourgear.auth.ProfileActivity;
+import com.group1.grabyourgear.common.AppConstants;
+import com.group1.grabyourgear.models.Equipment;
+import com.group1.grabyourgear.utils.CloudinaryUploader;
+import com.group1.grabyourgear.utils.UploadCallback;
+import com.group1.grabyourgear.utils.UserManager;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SupplierAddEquipmentActivity extends AppCompatActivity {
+
+    private EditText etName, etDescription, etPrice, etDiscount, etLocation;
+    private Spinner spinnerCategory;
+    private Button btnSaveImg;
+    private TextView tvUploadImgAddEquip;
+    private ImageView imgEquip;
+    private String imgUrl;
+    private CloudinaryUploader uploaderEquipImg;
+
+    private final ActivityResultLauncher<Intent> imagePickerLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent data = result.getData();
+                            uploaderEquipImg.handleResult(data);
+                        }
+                    }
+            );
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_supplier_add_equipment);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etName = findViewById(R.id.etEquipName);
+        etDescription = findViewById(R.id.etEquipDescription);
+        etPrice = findViewById(R.id.etEquipPrice);
+        etDiscount = findViewById(R.id.etEquipDiscount);
+        etLocation = findViewById(R.id.etEquipLocation);
+        spinnerCategory = findViewById(R.id.spCategoryAddEquip);
+        btnSaveImg = findViewById(R.id.btnSaveAddEquip);
+        tvUploadImgAddEquip = findViewById(R.id.tvEquipImageUpload);
+        imgEquip = findViewById(R.id.imgEquipment);
+
+        fillCategory();
+
+        uploaderEquipImg = new CloudinaryUploader(this, imagePickerLauncher);
+
+        btnSaveImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveEquipment();
+            }
+        });
+
+        tvUploadImgAddEquip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploaderEquipImg.pickImage(new UploadCallback() {
+                    @Override
+                    public void onSuccess(String imageUrl) {
+                        showImage(imageUrl);
+                        imgUrl = imageUrl;
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Toast.makeText(getApplicationContext(), "Upload failed：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void saveEquipment() {
+        String name = etName.getText().toString().trim();
+        String description = etDescription.getText().toString().trim();
+        String price = etPrice.getText().toString().trim();
+        String discount = etDiscount.getText().toString().trim();
+        String location = etLocation.getText().toString().trim();
+        Long selectedCategoryID = spinnerCategory.getSelectedItemId();
+        String imageUrl = imgUrl;
+
+        String supplierID = UserManager.getInstance().getUser().getUid();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("equipment");
+        String id = ref.push().getKey();
+
+        Equipment equipment = new Equipment(
+                id,
+                name,
+                Long.toString(selectedCategoryID),
+                supplierID,
+                Double.parseDouble(price),
+                Double.parseDouble(discount),
+                description,
+                imageUrl,
+                location,
+                0.0,
+                "available",
+                false
+        );
+
+        ref.child(id).setValue(equipment);
+
+    }
+
+    private void showImage(String url) {
+        Glide.with(this)
+                .load(url)
+                .placeholder(R.drawable.placeholder_avatar)
+                .into(imgEquip);
+
+        Glide.with(this)
+                .load(url)
+                .placeholder(R.drawable.placeholder_avatar)
+                .into(imgEquip);
+    }
+
+    private void fillCategory()
+    {
+        List<String> category = Arrays.asList(AppConstants.CurrentCategory.ALL, AppConstants.CurrentCategory.CONSTRUCTION, AppConstants.CurrentCategory.ELECTRONIC, AppConstants.CurrentCategory.OFFICE, AppConstants.CurrentCategory.VEHICLE);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                category
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(adapter);
+        spinnerCategory.setSelection(0);
+    }
+}
--- Grabyourgear/app/src/main/java/com/group1/grabyourgear/supplier/SupplierDashboardActivity.java
+++ Grabyourgear/app/src/main/java/com/group1/grabyourgear/supplier/SupplierDashboardActivity.java
@@ -1,6 +1,10 @@
 package com.group1.grabyourgear.supplier;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.group1.grabyourgear.R;
+import com.group1.grabyourgear.auth.ProfileActivity;
 import com.group1.grabyourgear.utils.BaseActivity;
+import com.group1.grabyourgear.utils.UserManager;
 
 public class SupplierDashboardActivity extends BaseActivity {
+
+    LinearLayout lyAddProduct, lyMyProducts, lyOrders, lyStoreProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +32,51 @@ public class SupplierDashboardActivity extends BaseActivity {
             return insets;
         });
 
+        lyAddProduct = findViewById(R.id.lyAddProduct);
+        lyMyProducts = findViewById(R.id.lyMyProducts);
+        lyOrders = findViewById(R.id.lyManageOrders);
+        lyStoreProfile = findViewById(R.id.lyStoreProfile);
+
         setHeaderTitle("Supplier Dashboard");
 
-//        if(UserManager.getInstance().isApproved()) {
-//            // show functionality
-//        } else {
-//            // show not approved message
-//        }
+        lyAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SupplierAddEquipmentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lyMyProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SupplierMyEquipmentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lyOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SupplierManageOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lyStoreProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        if(UserManager.getInstance().isApproved()) {
+            // show functionality
+
+        } else {
+            // show not approved message
+            Toast.makeText(getApplicationContext(), "Account pending approval!", Toast.LENGTH_LONG).show();
+        }
     }
 }
